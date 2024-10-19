ce4e4e7cd33cba748742beb3dca0486d5fae7d18
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-19 00:54:08 +0000

    node-sass 1.80.3

--- Formula/n/node-sass.rb
+++ Formula/n/node-sass.rb
@@ -1,8 +1,8 @@
 class NodeSass < Formula
   desc "JavaScript implementation of a Sass compiler"
   homepage "https://github.com/sass/dart-sass"
-  url "https://registry.npmjs.org/sass/-/sass-1.79.4.tgz"
-  sha256 "5521e75787930868404ba33e13ddab4b704e796af20d22c85a79f79d300e64b7"
+  url "https://registry.npmjs.org/sass/-/sass-1.80.3.tgz"
+  sha256 "34603949cfae96a8cc846c4cd910871d430e3dee08ceebfb0c36888637d81c89"
   license "MIT"
 
   bottle do
